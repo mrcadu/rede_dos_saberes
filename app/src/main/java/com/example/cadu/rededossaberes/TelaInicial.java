5e6bd4cc780ad515fbb265ef6c5a2dd45763a050
cadu <cadu@localhost.localdomain>
2017-10-11 12:26:36 -0300

    criei a tela inicial

---
+++ app/src/main/java/com/example/cadu/rededossaberes/TelaInicial.java
@@ -0,0 +1,29 @@
+package com.example.cadu.rededossaberes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.jar.Attributes;
+
+import static android.widget.Toast.*;
+import static com.example.cadu.rededossaberes.R.styleable.ActionBar_backgroundSplit;
+import static com.example.cadu.rededossaberes.R.styleable.View;
+
+public class TelaInicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_inicial);
+    }
+    public void verProjetos()
+    {
+
+    }
+
+
+}
