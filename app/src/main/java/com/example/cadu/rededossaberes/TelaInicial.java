5a7187d8c9343230bf1c59b95ecd7773860cbb5a
cadu <cadu@localhost.localdomain>
2017-10-16 17:43:00 -0200

    tela inicial quase pronta

--- app/src/main/java/com/example/cadu/rededossaberes/TelaInicial.java
+++ app/src/main/java/com/example/cadu/rededossaberes/TelaInicial.java
@@ -6,19 +6,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
+import android.support.v7.widget.Toolbar;
 import java.util.jar.Attributes;
-
 import static android.widget.Toast.*;
-import static com.example.cadu.rededossaberes.R.styleable.ActionBar_backgroundSplit;
-import static com.example.cadu.rededossaberes.R.styleable.View;
 
 public class TelaInicial extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tela_inicial);
     }
     public void verProjetos()
     {
