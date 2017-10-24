9d05ae1c9204188527b895045f0939986367f622
cadu <carlosvicc14@gmail.com>
2017-10-24 08:26:30 -0200

    adicionei mais duas activitys para os botões da tela inicial

--- app/src/main/java/com/example/cadu/rededossaberes/TelaInicial.java
+++ app/src/main/java/com/example/cadu/rededossaberes/TelaInicial.java
@@ -1,20 +1,29 @@
 package com.example.cadu.rededossaberes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuInflater;
 import android.view.Menu;
+import android.view.View;
 
 public class TelaInicial extends AppCompatActivity {
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_buttons, menu);
         return true;
     }
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+    }
+    public void ver_projetos(View view)
+    {
+        Intent mudarActivityVerProjetos = new Intent(this,VerProjetos.class);
+        startActivity(mudarActivityVerProjetos);
     }
 }
