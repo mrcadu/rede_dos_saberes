aacecf1cd11e6f90bdcfd4c24791f3dfd8bebf72
cadu <carlosvicc14@gmail.com>
2018-03-03 11:12:24 -0300

    generalizei todos os activitys para herdarem de uma classe que tenha a toolbar e os botões da toolbar para melhorar a modificação da classe

---
+++ app/src/main/java/com/example/cadu/rededossaberes/activity/ToolbarActivity.java
@@ -0,0 +1,42 @@
+package com.example.cadu.rededossaberes.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.example.cadu.rededossaberes.R;
+import com.parse.ParseUser;
+
+public class ToolbarActivity extends AppCompatActivity
+{
+    @Override
+    public void onContentChanged() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        super.onContentChanged();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_buttons, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch ( item.getItemId() )
+        {
+            case R.id.action_sair:
+                ParseUser.logOut();
+                Intent mudarActivityLogin = new Intent(this,Login.class);
+                startActivity(mudarActivityLogin);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/cadu/rededossaberes/adapter/CustomPrintAdapter.java
@@ -0,0 +1,8 @@
+package com.example.cadu.rededossaberes.adapter;
+
+/**
+ * Created by Cadu on 03/03/18.
+ */
+
+public class CustomPrintAdapter {
+}
